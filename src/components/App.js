be5b9adce685f81dbd5ac1afd2e1294b9060d889
SnigdhaSunnam <132300286+SnigdhaSunnam@users.noreply.github.com>
2023-12-17 09:29:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] =useState(0);
+
+  const handleNum1Change = (e) => {
+    setNum1{Number(e.target.value));
+           };
+    const handleNum2Change = (e) => {
+      setNum2{Number(e.target.value));
+             };
+      const handleAddition = () => {
+        setResult(num1 + num2);
+      };
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id="input1" type="number1" value= {num1}  onChange={handleNum1Change}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id="input2"  type= "number2" value= {num2} onChange={handleNum2Change}/> 
+
+    <button onClick = {handleAddition}>Add Numbers </button>
       
-      <p id='result'></p>
+      <p id="result">Result: {result}</p>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
